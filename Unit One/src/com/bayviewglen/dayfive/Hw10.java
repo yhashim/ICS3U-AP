8b48a33e1681b1372e8e12c6e6a7f831c4a42d12
yhashim <36962808+yhashim@users.noreply.github.com>
2018-09-19 13:51:17 -0400

    push

---
+++ Unit One/src/com/bayviewglen/dayfive/Hw10.java
@@ -0,0 +1,23 @@
+package com.bayviewglen.dayfive;
+
+import java.util.Scanner;
+
+public class Hw10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Input rectangle length: ");
+		double length = input.nextDouble(); 
+		System.out.println("Input rectangle width: ");
+		double width = input.nextDouble();
+		
+		double area = (length*width); 
+		double perimeter = ((2*length)+(2*width));
+		
+		System.out.println("The area of the rectangle is: " + ((Math.round(area))/10.0) + " meters squared");
+		System.out.println("The perimeter of the rectangle is: " + ((Math.round(perimeter))/10.0) + " meters squared");
+	}
+
+}
